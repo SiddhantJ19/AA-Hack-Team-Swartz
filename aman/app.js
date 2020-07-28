b3161a3c11239e97cb16c82f444e47b0c0354357
Siddhant Jain <mail.siddhantjain19@gmail.com>
2020-07-28 18:17:07 +0530

    corrected input field names in mockControllerRegister

--- aman/app.js
+++ aman/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+
 var bodyParser = require('body-parser');
 var router = require('./index.js');
 
--- aman/src/controller/mockController.js
+++ aman/src/controller/mockController.js
@@ -1,18 +1,21 @@
 const puppeteer = require('puppeteer');
 
 const mockControllerRegister = async (req, res) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-
-    await page.goto('https://finvu.in/webview/onboarding/webview-register', {waitUntil: 'load'});
+    await page.goto('https://finvu.in/webview/onboarding/webview-register', {waitUntil: 'load', timeout: 0});
     console.log(page.url());
+    // console.log(await page.content()); // stringified snapshot of fom
 
+    await page.waitForSelector('input[formcontrolname="aaId"]', {timeout : 0});
+    await page.type('input[formcontrolname="aaId"]', "dummyId@AA", {delay: 5});
+    await page.type('input[formcontrolname="mobile"]', "8433460645");
+    await page.type('input[formcontrolname=password]', "1234");
+    await page.type('input[formcontrolname=reEnterPassword]', "1234");
 
-    await page.type('input[formControlName=aaId]', "");
-    await page.type('input[formControlName=mobile]', "");
-    await page.type('input[formControlName=password]', "");
-    await page.type('input[formControlName=confirmPassowwrd]', "");
-    await page.type('input[formControlName=aaId]', "");  // check box for accept terms and conditions
+    await page.evaluate(() => { // checkbox for terms and conditions
+        document.querySelector("input[formcontrolname=termCondition]").parentElement.click();
+    }); 
 
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
@@ -25,7 +28,7 @@ const mockControllerRegister = async (req, res) => {
 };
 
 const mockControllerLogin = async (req, res) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     console.log("yahan tak aaye hain")
     await page.goto('https://finvu.in/webview/onboarding/webview-login', {waitUntil: 'load', timeout: 0});
