f611466c811503500c4d04d18a69663d3554b61b
Vineet Sharma <vineet.s17@iiits.in>
2020-07-31 22:36:23 +0530

    Update i18n.js

--- web/src/plugins/i18n.js
+++ web/src/plugins/i18n.js
@@ -7,14 +7,18 @@ const messages = {
     'en': {
         welcomeMsg: 'Fasttrack user onboarding on AA',
         subHeading: 'Verified Agent Portal',
-        email: 'email',
+        email: 'Email',
         password: 'password',
         login: 'login',
+        logout: 'logout',
+        register: 'Register',
         dashboardHeader: 'Consent Applicant List',
         phoneNo: 'Phone Number',
+        statusText: 'status',
         status: 'Applied',
         btnText: 'Create',
         registerText: "Doesn't have an account. Click here to register",
+        loginText: 'Already have an account. Click here to login',
         createApplicantBtn: 'Add Applicant'
     },
     'hi': {
@@ -23,11 +27,15 @@ const messages = {
         email: 'ईमेल',
         password: 'कुंजिका',
         login: 'लॉग इन करें',
+        logout: 'लॉग आउट',
+        register: 'रजिस्टर करें',
         dashboardHeader: 'सहमति आवेदक सूची',
         phoneNo: 'फ़ोन नंबर',
+        statusText: 'स्थिति',
         status: 'लागू',
         btnText: 'बनाएं',
         registerText: 'खाता नहीं है पंजीकरण करने के लिए यहां क्लिक करें',
+        loginText: 'क्या आपके पास पहले से एक खाता मौजूद है। लॉग इन करने के लिए यहां क्लिक करें',
         createApplicantBtn: 'आवेदक जोड़ें'
     }
 };
